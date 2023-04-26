8f663b287ea4f74910c3870e7fbeffb002d23a2f
khanhtran <khanh.tran@firstpage.vn>
2023-04-26 14:23:24 +0700

    Correct way to listen on tab reload

--- src/background.ts
+++ src/background.ts
@@ -63,8 +63,9 @@ chrome.tabs.onRemoved.addListener((tabId, info) => {
     removeTabData(tabId);
 });
 
-chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
-    if (changeInfo.url === undefined) removeTabData(tabId);
+chrome.webNavigation.onCommitted.addListener((details) => {
+    if (details.transitionType.indexOf('reload') > -1)
+        removeTabData(details.tabId);
 });
 // async function RunFetch(urls: Array<string>) {
 //     for (let i = 0; i < urls.length; i++) {}
